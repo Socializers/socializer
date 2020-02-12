f5862c636ec758ed0b4dbb4f6377a566ac94a6d2
daniaalrababa9 <rababadania@gmail.com>
2020-02-12 13:52:45 +0200

    add

---
+++ src/models/educations/educations-model.js
@@ -0,0 +1,9 @@
+'use strict';
+const schema = require('./educations-schema.js');
+const Model = require('../mongo.js');
+class Educations extends Model{
+    constructor(){
+        super(schema);
+    }
+}
+module.exports= new Educations;
